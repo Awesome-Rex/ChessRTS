dcc63ee0edcbec4640fa398c6dfcf70031a8cbe8
Awesome-Rex <john.esqqq@gmail.com>
2019-01-19 18:03:31 -0500

    First Unity Project Commit!!!
    :open_mouth: :open_mouth:

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Selectable : MonoBehaviour
+{
+    public bool selected;
+
+    public UnityEvent selectEvents;
+
+    public void select() {
+        selected = true;
+
+        selectEvents.Invoke();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
